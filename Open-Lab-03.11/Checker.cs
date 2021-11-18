acdbb6f6349ceef220f4e700e072d618537f0d3b
Ján Kubaška <jajanokubaska@gmail.com>
2021-11-18 18:58:00 +0100

    Implemented the method that checks if a string is a palindrome.

--- Open-Lab-03.11/Checker.cs
+++ Open-Lab-03.11/Checker.cs
@@ -6,7 +6,21 @@ namespace Open_Lab_03._11
     {
         public bool IsPalindrome(string str)
         {
-            throw new NotImplementedException();
+            string  revs = "";
+           
+            for (int i = str.Length - 1; i >= 0; i--) 
+            {
+                revs += str[i].ToString();
+            }
+            if (revs == str) 
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
         } 
     }
 }
